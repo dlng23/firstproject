a206614d3faab47bc337ba4058d14e642665fcfe
dlng23 <1myungjin@ewhain.net>
2025-01-18 00:26:52 +0900

    15장~16장

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,8 @@ import java.util.List;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm(){
@@ -45,10 +49,10 @@ public class ArticleController {
         log.info("id = " + id);
         // 1. id를 조회해 DB에서 해당 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
-
+        List<CommentDto> commentsDtos = commentService.comments(id);
         // 2. 가져온 데이터를 모델에 등록하기
         model.addAttribute("article", articleEntity);
-
+        model.addAttribute("commentDtos", commentsDtos);
         // 3. 조회한 데이터를 사용자에게 보여 주기 위한 뷰 페이지 만들고 반환하기
         return "articles/show";
     }
